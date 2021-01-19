90e0bfe682c1eda4fda3340aee9894712b991049
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-01-19 23:52:09 +0530

    updated files

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react' ;
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello my name is Bhavish.good morning!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+constructor() {
+  super();         //super()
+
+  this.state = {
+    monsters : []
+  };
+}
+//now using lifecycle method
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  //taking a response and converting it into json that javascript can understand
+  .then(response => response.json())
+  //taking that users that wer got back then and setting out monsters to that arrtay of users
+  .then(users => this.setState({monsters : users}));
+}
+
+  render() {
+    return(
+      <div className="App">
+        {this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> { monsters.name } </h1>))}
     </div>
-  );
+    );
+
+  }
 }
 
 export default App;
