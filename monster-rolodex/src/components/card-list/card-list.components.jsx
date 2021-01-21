9e645ca8d9a9087144e254885c8413ca4dab634d
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-01-21 15:34:59 +0530

    added styling

--- monster-rolodex/src/components/card-list/card-list.components.jsx
+++ monster-rolodex/src/components/card-list/card-list.components.jsx
@@ -4,8 +4,8 @@ import './card-list.style.css'
 
 export const CardList = props => (
     <div className='card-list'>
-        {props.monsters.map(monsters => (
-        <h1 key={monsters.id}> { monsters.name } </h1>
+        {props.monsters.map(monster => (
+        <Card key={monster.id} monster = {monster}/>
         ))}
     </div>
 ); 
--- monster-rolodex/src/components/card/card.component.jsx
+++ monster-rolodex/src/components/card/card.component.jsx
@@ -6,4 +6,4 @@ export const Card = props => (
     <div className="card-container">
         <h1> {props.monster.name} </h1>
     </div>
-); 
+);
